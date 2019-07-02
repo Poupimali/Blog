f34eba010a8852e7aff7b046f492ff4b29b5e501
Poupimali <poupimali@gmail.com>
2019-07-02 23:48:40 +0200

    flash message rouge et vert ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -35,7 +35,7 @@ class AppFixtures extends Fixture
      */
     public function load( ObjectManager $manager)
     {
-        for ($i = 1; $i <= 25; $i++) {
+        for ($i = 1; $i <= 1000; $i++) {
             $category = new Category();
             $category->setName("category " . $i);
             $manager->persist($category);
